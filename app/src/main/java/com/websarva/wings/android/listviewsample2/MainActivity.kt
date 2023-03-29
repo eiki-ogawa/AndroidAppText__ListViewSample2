394a81ac7d8cff00f95747ef850d5696dfdd5971
eiki-ogawa <eiki-ogawa@x-power.co.jp>
2023-03-29 10:23:30 +0900

    init

---
+++ app/src/main/java/com/websarva/wings/android/listviewsample2/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.websarva.wings.android.listviewsample2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        var menuList = mutableListOf("唐揚げ定食", "ハンバーグ定食", "生姜焼き定食", "ステーキ定食")
+        val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
+        lvMenu.adapter = adapter
+        lvMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val dialogFragment = OrderConfirmDialogFragment()
+            dialogFragment.show(supportFragmentManager, "OrderConfirmDialogFragment")
+        }
+    }
+}
